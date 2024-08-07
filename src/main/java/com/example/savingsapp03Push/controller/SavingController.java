ab33766021f404bca85c0ecb8cdd855caaddf891
WingYanLo <low3@student.douglascollege.ca>
2024-08-07 10:47:47 -0700

    Added GitHub repository link to main controller

--- src/main/java/com/example/savingsapp03Push/controller/SavingController.java
+++ src/main/java/com/example/savingsapp03Push/controller/SavingController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+// GitHub Repository: https://github.com/WingYanLo/CSIS3275SavingsApp03Push
+
 @Controller
 public class SavingController {
 
